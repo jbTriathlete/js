3eebb4071dfc3a7ab8644fb707336fed8fc34116
jbtriathlete <jonbakerappdev@gmail.com>
2015-10-15 23:59:36 -0500

    ready

--- js/WebForm1.aspx.vb
+++ js/WebForm1.aspx.vb
@@ -1,4 +1,6 @@
-﻿Public Class WebForm1
+﻿
+
+Public Class WebForm1
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -6,6 +8,7 @@
     End Sub
 
     Protected Sub text_TextChanged(sender As Object, e As EventArgs)
+        Dim x As New clssql_customer
 
     End Sub
 End Class
